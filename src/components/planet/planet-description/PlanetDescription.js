029ebbd0d485b37105e9d5b90c12d015c5ac246d
Kristi <kristiana.d.ingco@gmail.com>
2022-01-27 19:47:42 -0800

    Modify styles for planet description

--- src/components/planet/planet-description/PlanetDescription.js
+++ src/components/planet/planet-description/PlanetDescription.js
@@ -1,11 +1,15 @@
 import React from "react";
+import "./PlanetDescription.scss";
 
 const PlanetDescription = ({ name, content, source }) => {
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{content}</p>
-      <span>Source: {source}</span>
+    <div className="planet-description">
+      <h1 className="planet-description-header">{name}</h1>
+      <p className="planet-description-content">{content}</p>
+      <span className="planet-description-source">
+        Source: <a href={source}>Wikipedia</a>
+        <img src="assets/icon-source.svg" alt="source" />
+      </span>
     </div>
   );
 };
