fd7d03e15f84a91ac957b2b02e3bd7ff0129866e
Kristi <kristiana.d.ingco@gmail.com>
2022-02-04 17:37:06 -0800

    Add navigation list

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Navigation.scss";
+import { useLocation } from "react-router-dom";
 import NavigationList from "./navigation-list/NavigationList";
 
 const Navigation = ({ data }) => {
+  const location = useLocation();
+  const [toggle, setToggle] = useState("toggle-menu");
+
+  useEffect(() => {
+    setToggle("toggle-menu");
+  }, [location]);
+
+  const openOrClose = () => {
+    if (toggle === "toggle-menu") {
+      setToggle("");
+    } else {
+      setToggle("toggle-menu");
+    }
+  };
+
   return (
-    <div>
-      The Planets <NavigationList data={data} />
+    <div className="navigation">
+      <h2 className="navigation-header">The Planets</h2>{" "}
+      <img src="/assets/icon-hamburger.svg" alt="menu" onClick={openOrClose} />
+      <NavigationList data={data} toggle={toggle} />
     </div>
   );
 };
--- src/components/navigation/navigation-list/NavigationList.js
+++ src/components/navigation/navigation-list/NavigationList.js
@@ -1,17 +1,32 @@
 import React from "react";
+import "./NavigationList.scss";
 import { Link } from "react-router-dom";
 
-const NavigationList = ({ data }) => {
+const NavigationList = ({ data, toggle }) => {
   return (
-    <div>
-      {data.map((planet) => (
-        <Link
-          key={planet.name.toLowerCase()}
-          to={`/${planet.name.toLowerCase()}`}
-        >
-          {planet.name}
-        </Link>
-      ))}
+    <div className={`navigation-list ${toggle}`}>
+      <div className="navigation-links">
+        {data.map((planet) => (
+          <div className={"navigation-link "}>
+            <Link
+              className="navigation-link-planet"
+              key={planet.name.toLowerCase()}
+              to={`/${planet.name.toLowerCase()}`}
+            >
+              <div className="navigation-link-planet-info">
+                <span className={`dot dot-${planet.name.toLowerCase()}`}></span>
+                <span className="navigation-planet-name">{planet.name}</span>
+              </div>
+
+              <img
+                className="navigation-link-chevron"
+                src="assets/icon-chevron.svg"
+                alt="chevron"
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
